741b8a148b218dd053f855968caed9658f163f29
Akihito Koriyama <akihito.koriyama@gmail.com>
2025-04-03 13:48:55 +0900

    Add custom exception classes for EnvJson handling
    Introduce four exception classes to improve error handling: `InvalidEnvJsonFormatException`, `InvalidIniFileException`, `InvalidJsonContentException`, and `JsonFileNotReadableException`. These specific exceptions enhance clarity and provide more granular error reporting.

---
+++ src/Exception/InvalidJsonContentException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Koriym\EnvJson\Exception;
+
+use LogicException;
+
+final class InvalidJsonContentException extends LogicException
+{
+}
---
+++ src/Exception/JsonFileNotReadableException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Koriym\EnvJson\Exception;
+
+use RuntimeException;
+
+final class JsonFileNotReadableException extends RuntimeException
+{
+}
